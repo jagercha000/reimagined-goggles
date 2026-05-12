998f5f5fb39eadb1d7a4cdbef654272b43b322d7
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-12 15:57:49 -0500

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,18 @@
 import "./header.js"
 import "./footer.js"
 import "./loader.js"
+var sitePath = "/reimagined-goggles/";
+async function loadComponents() {
+  var headerResponse = await fetch(sitePath + "components/header.html");
+  var footerResponse = await fetch(sitePath + "components/footer.html");
+  var headerContent = await headerResponse.text();
+  var footerContent = await footerResponse.text();
+  document.querySelector('header').innerHTML = headerContent;
+  document.querySelector('footer').innerHTML = footerContent;
+}
+function hideLoader() {}
+async function init() {
+  await loadComponents();
+  hideLoader();
+}
+init();
