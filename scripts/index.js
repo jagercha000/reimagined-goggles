67515eeb73574adb663070ed0bff507743dc6719
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-13 09:45:19 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,5 @@
-
+window.addEventListener('load', function() {
+  document.querySelector('.home-hero-button').addEventListener('click', function() {
+    window.location.href = 'overview.html';
+  });
+});
