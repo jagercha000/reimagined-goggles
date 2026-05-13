5354d33946b8784d827a619fdeb4d22c40375e84
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-13 14:05:16 -0500

    Update overview.js

--- scripts/overview.js
+++ scripts/overview.js
@@ -1,3 +1,6 @@
 window.addEventListener('load', function() {
   createPlayer('gallery', document.querySelector('.gallery-player'), 600, 375);
+  document.querySelector('.abiotic-factors-button').addEventListener('click', function() {
+    window.location.href = 'abiotic-factors.html';
+  });
 });
