15c40f03fe072d216d8a2493f9b93801dbd7eaff
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-13 09:18:51 -0500

    Update player.js

--- scripts/player.js
+++ scripts/player.js
@@ -1,8 +1,10 @@
 import "./constants.js"
-globalThis.createPlayer = function(id, target) {
+globalThis.createPlayer = function(id, target, width, height) {
   var element = document.createElement('iframe');
   var playerURL = sitePath + "player/index.html?id=" + id;
   element.setAttribute('src', playerURL);
-  element.setAttribute('class', 'player-frame');
+  element.setAttribute('class', 'player-frame');e
+  element.setAttribute('width', width);
+  element.setAttribute('height', height);
   target.appendChild(element);
 }
