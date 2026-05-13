5ee553ea34bd8e886579887228848145e7026d11
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-12 20:55:45 -0500

    Update loader.js

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,5 +1,10 @@
 globalThis.hooks = globalThis.hooks || new Object();
-globalThis.hooks['loader-init'] = function() {};
+globalThis.hooks['loader-init'] = function() {
+  globalThis.loadingBar = new ldBar('.loader', { 'preset': 'circle', 'value': 0, 'stroke': '#ffffff', 'stroke-width': 3 });
+};
+globalThis.hooks['loader-update'] = function(percent) {
+  globalThis.loadingBar.set(percent, true);
+};
 globalThis.hooks['loader-hide'] = function() {
   setTimeout(function() {
     gsap.fromTo('.loader-overlay', { opacity: 0 }, { opacity: 1, duration: 2, onComplete: function() {
