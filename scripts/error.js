1353c77bef285d9e5426ce935e108c862413fa0a
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-14 12:24:50 -0500

    Update error.js

--- scripts/error.js
+++ scripts/error.js
@@ -1,11 +1,23 @@
 import "./constants.js"
-window.addEventListener('load', async function() {
-  var result = await fetch(sitePath + "styles/error.css");
+function addStyle(url) {
+  var result = await fetch(url);
   var content = await result.text();
   var element = document.createElement('style');
   element.textContent = content;
   element.setAttribute('type', 'text/css');
   document.head.appendChild(element);
+}
+function addScript(url) {
+  var element = document.createElement('script');
+  element.setAttribute('src', url);
+  element.setAttribute('type', 'text/javascript');
+  document.head.appendChild(element);
+}
+window.addEventListener('load', async function() {
+  addStyle(sitePath + "styles/global.css");
+  addStyle(sitePath + "styles/error.css");
+  addScript(sitePath + "lib/gsap.js");
+  addScript(sitePath + "scripts/global.js");
   document.querySelector('.error-back-button').addEventListener('click', function() {
     window.history.go(-1);
   });
