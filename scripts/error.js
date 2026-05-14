fde5c65d959ba9cad1b261a97f49150898db3dc8
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-14 12:30:08 -0500

    Update error.js

--- scripts/error.js
+++ scripts/error.js
@@ -1,15 +1,15 @@
 import "./constants.js"
-function addStyle(url) {
-  var result = await fetch(url);
+function addStyle(styleURL) {
+  var result = await fetch(styleURL);
   var content = await result.text();
   var element = document.createElement('style');
   element.textContent = content;
   element.setAttribute('type', 'text/css');
   document.head.appendChild(element);
 }
-function addScript(url) {
+function addScript(scriptURL) {
   var element = document.createElement('script');
-  element.setAttribute('src', url);
+  element.setAttribute('src', scriptURL);
   element.setAttribute('type', 'text/javascript');
   document.head.appendChild(element);
 }
