0359caf82a06b582f3e4dc180d02a6d6e714fefa
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-14 09:29:35 -0500

    Update climate.js

--- player/players/climate.js
+++ player/players/climate.js
@@ -6,3 +6,18 @@ element.innerText = "Loading";
 document.body.appendChild(element);
 globalThis.player.climateData.images.winter = await globalThis.player.util.downloadImage("climate/winter.jpg");
 globalThis.player.climateData.images.summer = await globalThis.player.util.downloadImage("climate/summer.jpg");
+globalThis.player.climateData.currentSeason = "winter";
+async function climateFrame() {
+  if(globalThis.player.climateData.currentSeason == "winter") {
+    globalThis.player.util.fitImage(globalThis.player.climateData.images.winter);
+  } else if(globalThis.player.climateData.currentSeason == "summer") {
+    globalThis.player.util.fitImage(globalThis.player.climateData.images.summer);
+  }
+  window.requestAnimationFrame(climateFrame);
+}
+window.requestAnimationFrame(climateFrame);
+setTimeout(function() {
+  gsap.fromTo('.climate-loading', { opacity: 1 }, { opacity: 0, duration: 1, onComplete: function() {
+    document.querySelector('.climate-loading').classList.add('hidden');
+  }});
+}, 15000);
